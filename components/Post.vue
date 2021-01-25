c77df9ca7a8eb2618e46cc85797838a47acf827e
fmabid <fmaabid@gmail.com>
2021-01-25 14:20:51 +0600

    image component

---
+++ components/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src='src' alt="" class="inset-0 w-full h-full object-cover mb-4" />
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    src: {
+      default: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
