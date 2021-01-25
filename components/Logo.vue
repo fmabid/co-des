0dbb26de704e41e59c04cc9926b837fc1169cfa5
fmabid <fmaabid@gmail.com>
2021-01-25 11:51:33 +0600

    Added logo.

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,29 +1,11 @@
 <template>
-  <svg class="NuxtLogo" width="245" height="180" viewBox="0 0 452 342" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z"
-      fill="#00C58E"
-    />
-    <path
-      d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z"
-      fill="#108775"
-    />
-    <path
-      d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z"
-      fill="#2F495E"
-    />
+  <svg xmlns="http://www.w3.org/2000/svg" width="192.88" height="36" viewBox="0 0 192.88 36">
+    <g id="Group_289" data-name="Group 289" transform="translate(-788.959 -465.142)">
+      <path id="Path_725" data-name="Path 725" d="M793.614,469.8H820.3v6.661h4.655V465.142h-36v24.491l4.655-4.7Z" fill="#e17800"/>
+      <path id="Path_726" data-name="Path 726" d="M820.3,496.6v4.73H793.614v-4.941l-1.49,1.506-3.164-3.131v11.22h36V496.6Z" transform="translate(0 -4.839)" fill="#e17800"/>
+      <path id="Path_727" data-name="Path 727" d="M816.858,480.209l-7.839,6.043L802.25,476l-7.728,7.81v6.618l7-7.077L807.9,493l8.11-6.253,5.208,6.789v-7.647Z" transform="translate(-0.909 -1.774)" fill="#e17800"/>
+      <path id="Path_728" data-name="Path 728" d="M793.614,495.41v-6.618l-4.655,4.7v.289l3.164,3.131Z" transform="translate(0 -3.864)" fill="#e17800"/>
+      <text id="CLICK_MOTTO" data-name="CLICK MOTTO" transform="translate(831.839 490.451)" fill="#e17800" font-size="22" font-family="Helvetica"><tspan x="0" y="0">CLICK MOTTO</tspan></text>
+    </g>
   </svg>
 </template>
-
-<style>
-.NuxtLogo {
-  animation: 1s appear;
-  margin: auto;
-}
-
-@keyframes appear {
-  0% {
-    opacity: 0;
-  }
-}
-</style>
